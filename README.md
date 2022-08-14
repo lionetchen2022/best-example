77408a944130613e6372a45efcbcf77f28fce794
LionetChen <chinalionet@gmail.com>
2022-08-14 16:55:40 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# The Best Example Ever
+Which will be a part of the best commit ever
---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener(
+    "DOMContentLoaded",
+    function (event) {
+        alert("Page is loaded on the event of " + event);
+    }
+)
